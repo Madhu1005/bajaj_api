3101e91b017410e9a4658f0896e9a10ae528dbf2
Madhu1005 <123300469+Madhu1005@users.noreply.github.com>
2025-08-29 11:33:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-
+import os
 app = FastAPI()
 
 class InputData(BaseModel):
@@ -45,3 +45,9 @@ async def bfhl(input_data: InputData):
         "sum": str(total_sum),
         "concat_string": alternating_caps
     }
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
